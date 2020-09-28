b8462c7f12672f97602b6156b0a3585a7a1ad465
Laxya Pahuja <laxya.pahuja8@gmail.com>
2020-09-28 22:52:19 +0530

    10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fontin",
-    version="0.0.9",
+    version="0.0.10",
     author="Laxya Pahuja",
     author_email="laxya.pahuja8@gmail.com",
     description="A better font extractor and installer for bulk fonts in one archive.",
